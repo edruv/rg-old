e28af3991da3fbd961e1c88b2fe4aa8048f305d8
Unknown <edson.ruvalcaba@alumnos.udg.mx>
2018-10-18 11:26:40 -0500

    mejorando agregarCliente

--- php/agregarCliente.php
+++ php/agregarCliente.php
@@ -2,15 +2,15 @@
 	require_once 'config.php';
 	require_once 'methods.php';
 	if(isset($_POST['btnRegister'])){
-		$nombre = $_POST['campoNombre'];
-		$apellidom = (empty($_POST['campoApellidoMaterno'])) ? "" : $_POST['campoApellidoMaterno'];
-		$apellidop = $_POST['campoApellidoPaterno'];
-		$calle = $_POST['campoCalle'];
-		$municipio = $_POST['Municipio'];
+		$nombre = strtolower($_POST['campoNombre']);
+		$apellidom = (empty($_POST['campoApellidoMaterno'])) ? "" : strtolower($_POST['campoApellidoMaterno']);
+		$apellidop = strtolower($_POST['campoApellidoPaterno']);
+		$calle = strtolower($_POST['campoCalle']);
+		$municipio = strtolower($_POST['Municipio']);
 		$ext = $_POST['campoExt'];
 		$interior = (empty($_POST['campoInt'])) ? "" : $_POST['campoInt'];
 		$telefono = $_POST['campoTelefono'];
-		$colonia = $_POST['campoColonia'];
+		$colonia = strtolower($_POST['campoColonia']);
 		$llave = $nombre.".".$apellidop.".".$apellidom;
 		$status = array();
 
